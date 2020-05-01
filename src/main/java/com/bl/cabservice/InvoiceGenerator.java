19c228d222e8f0e309bf6f977f97acebd2b0249a
priyankagarad <garadpriyanka1995@gmail.com>
2020-05-01 13:48:34 +0530

    ADDED:Welcome Message

---
+++ src/main/java/com/bl/cabservice/InvoiceGenerator.java
@@ -0,0 +1,7 @@
+package com.bl.cabservice;
+public class InvoiceGenerator {
+    public static void main(String args[])
+    {
+        System.out.println("Welcome to Cab Invoice Generator ");
+    }
+}
